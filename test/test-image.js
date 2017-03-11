612667e4578fb5899a8ba849a760f41d3a31d800
Rakesh Patel <emailme@rakeshrpatel.com>
2017-03-11 18:44:17 -0500

    component tested

--- test/test-image.js
+++ test/test-image.js
@@ -1,7 +1,18 @@
 const 
 	React = require('react'),
 	TestUtils = require('react-addons-test-utils'),
-	should = require('chai').should();
+	should = require('chai').should(),
+	ArtImage = require('../app/components/ArtImage');
 
-var 
-
+describe('ArtImage component', function() {
+	it ('Renders the image', function() {
+		var url = "http://www.fridakahlo.it/files_eventi/15/scheda/37_frida_portrait";
+		
+		var renderer = TestUtils.createRenderer();
+		renderer.render(<ArtImage imageUrl ={url} />);
+		var result = renderer.getRenderOutput();
+		console.log(result.type);
+		result.type.should.equal('img');
+		result.props.src.should.equal(url);
+	});
+});
