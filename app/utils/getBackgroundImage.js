14324fc2188e6eb58841a82bbc93d95050b5e346
Rakesh Patel <emailme@rakeshrpatel.com>
2017-03-13 15:20:56 -0400

    axios worked for post to artsy API

--- app/utils/getBackgroundImage.js
+++ app/utils/getBackgroundImage.js
@@ -1,17 +1,27 @@
 const axios = require('axios');
 
 var backgroundImage = {
-    getBackgroundImage() {
-        fetch('https://api.artsy.net/api/tokens/xapp_token', myInit) // Call the fetch function passing the url of the API as a parameter
-            .then(function(response) {
-                console.log(response);
-            })
-            .catch(function(err) {
-                // This is where you run code if the server returns any errors
-            })
-    },
+    // getBackgroundImage() {
+    //     fetch('https://api.artsy.net/api/tokens/xapp_token', myInit) // Call the fetch function passing the url of the API as a parameter
+    //         .then(function(response) {
+    //             console.log(response);
+    //         })
+    //         .catch(function(err) {
+    //             // This is where you run code if the server returns any errors
+    //         })
+    // },
     getBackgroundImageAxios() {
-        return axios.get('https://api.github.com/users/rakeshpatel87p')
+        return axios.post('https://api.artsy.net/api/tokens/xapp_token', {
+        	client_id: "cd7051715d376f899232", 
+    		client_secret: "de9378d3d12c2cbfb24221e8b96d212c"
+        })
+        .then(function(info){
+        	console.log(info);
+        	console.log(info.data.token)
+        })
+        .catch(function(error){
+        	console.log(error)
+        })
     }
 
 };
