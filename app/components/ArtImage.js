6f79f57f723f115fc4b3ddb277f58fd0b4d837f9
Rakesh Patel <emailme@rakeshrpatel.com>
2017-03-13 14:21:32 -0400

    hooking up client api request

--- app/components/ArtImage.js
+++ app/components/ArtImage.js
@@ -1,10 +1,12 @@
 const React = require('react');
+const backgroundImage = require('../utils/getBackgroundImage')
 
 const ArtImage = React.createClass({
 	render(){
+		console.log(backgroundImage.getBackgroundImage());
 		return (
 			<img src={this.props.imageUrl} />
-			);
+		);
 	}
 });
 
